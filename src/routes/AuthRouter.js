4e2843bb9a3e2d09663740055e2d44bc3f67ed16
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2020-04-01 18:41:54 +0600

    moved login to auth router

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,10 +1,84 @@
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
+
+const loggerService = require('../services/LoggingService');
+const userService = require('../services/userService');
+const authService = require('../services/AuthService');
 
 router.get('/ink', (req, res, next) => {
 	console.log(' This is auth routes /ink');
 	res.send('AUth Route');
 
 });
+
+router.post('/login', async function(req, res, next) {
+	let response = {};
+	try {
+		console.log(req.body);
+		let email = req.body.email || "", pass = req.body.pass || "";
+		if(!_.isEmpty(email) && !_.isEmpty(pass)) {
+			let result = await userService.findUserByEmailAndPassword(email, pass);
+			if(result.success === false ) {
+				loggerService.getDefaultLogger().error('[ROUTE]-[USER]-ERROR: Query Failed at /auth/login route: ');
+			} else {
+				let user = result.result;
+				let token = await authService.getTokenWithExpireTime(result.result.email, result.result.password, 60*60);
+				response = {
+					name: user.name,
+					role: user.role,
+					_id: user._id,
+					email: user.email,
+					lastLogin: user.lastLogin,
+					token: token.token
+				} ;
+				console.log('response with token: ', response);
+				res.set({
+					'X-Auth-Token': token.token
+				});
+			}
+		} else {
+			response = {};
+		}
+
+	} catch (ex) {
+		console.log(ex);
+		loggerService.getDefaultLogger().error('[ROUTE]-[INDEX]-ERROR: Exception get request at /auth/login route: ' + JSON.stringify(ex));
+	}
+
+	res.send(response);
+});
+
+
+// router.post('/login', async function(req, res, next) {
+// 	let response = {};
+// 	try {
+// 		console.log(req.body);
+// 		let email = req.body.email || "", pass = req.body.pass || "";
+// 		//loggingService.getDefaultLogger().info('Reached Login route');
+// 		if(!_.isEmpty(email) && !_.isEmpty(pass)) {
+// 			let result = await userService.getUser(email, pass);
+// 			console.log(result);
+// 			if(result.success === false ) {
+// 				loggerService.getDefaultLogger().error('[ROUTE]-[USER]-ERROR: Query Failed at /auth/login route: ');
+// 			} else {
+// 				response = {
+// 					name: result.data.name,
+// 					email: result.data.email,
+// 					lastLogin: result.data.lastLogin,
+// 					token: result.token
+// 				}
+// 			}
+// 		} else {
+// 			response = {};
+// 		}
+//
+// 	} catch (ex) {
+// 		console.log(ex);
+// 		loggerService.getDefaultLogger().error('[ROUTE]-[INDEX]-ERROR: Exception get request at /auth/login route: ' + JSON.stringify(ex));
+// 	}
+//
+// 	res.send(response);
+// });
 
 module.exports = router;
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,15 +1,15 @@
 const session = require('express-session');
 const ConfigLoader = require('../config/ConfigLoader');
-const JwtService = require('../services/JwtService');
+const jwtService = require('../services/JwtService');
 const _ = require('lodash');
 const config = new ConfigLoader();
 
+module.exports = class AuthService {
 
-class AuthService {
 	constructor() {
 		this.sessionStore = new session.MemoryStore();
 	}
-	getSessionStore() {
+	static getSessionStore() {
 		return this.sessionStore;
 	}
 
@@ -20,7 +20,7 @@ class AuthService {
 				return response;
 			}
 			let token = req.header.authorization.slice(7);
-			let verified = await JwtService.verify(token);
+			let verified = await jwtService.verify(token);
 			response = true;
 			console.log('verified: ', verified);
 		} catch (ex) {
@@ -29,8 +29,32 @@ class AuthService {
 		return response;
 	}
 
-}
+	static async getToken(user, pass) {
+		var user = { user, pass };
+		const token = jwt.sign({user}, config.secret);
 
-const authService = new AuthService();
+		console.log("Token Created => " , token);
+		return token;
+	}
 
-module.exports = authService;
+	static async getTokenWithExpireTime(user, pass, time) {
+		if (time == null || time == undefined) {
+			time = config.tokenExpiryTime;
+		}
+		// var user = { user, pass };
+		// const token = jwt.sign({user}, config.secret, {
+		// 	expiresIn: time // in sec.
+		// });
+
+		let signedToken = await jwtService.sign({
+			email: user,
+			pass: pass
+		}, config.secret, {
+			expiresIn: time // in sec.
+		});
+
+		console.log("Token Created => " , signedToken);
+		return signedToken;
+	}
+
+};
