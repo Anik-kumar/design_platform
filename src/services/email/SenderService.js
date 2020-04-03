97c1d40b74bee5ea703cd456f6f7318b4457cafc
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2020-04-03 13:10:33 +0600

    created email service

---
+++ src/services/email/SenderService.js
@@ -0,0 +1,19 @@
+// API Key: 530f096420d35de2e3711dc20a7b5de8
+// https://app.sender.net/api
+
+
+/*
+https://help.sender.net/knowledgebase/id-like-to-use-the-api-is-there-anything-to-help-me-get-started/
+
+{
+    "method": "sendTransactionalEmail",
+    "params": {
+        "api_key": "your_api_key_here",
+        "transactional_campaign_id": "your_campaign_id",
+        "email": "recipient_email",
+        "personalized_data": {"fieldName": "fieldValue", ...},
+        "attachments": {"fileName": "fileUrl", ...}
+    }
+}
+
+ */
